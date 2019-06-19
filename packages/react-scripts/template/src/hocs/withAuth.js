3365a23aafbab937592fb3ee43f59f018e1efc62
Mehdi GRAR <mehdi.grar@gmail.com>
2019-06-19 16:08:09 +0200

    wip

---
+++ packages/react-scripts/template/src/hocs/withAuth.js
@@ -0,0 +1,91 @@
+import React, { Component, createContext } from 'react'
+import { Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import withAPI from './withAPI'
+
+const AuthContext = createContext()
+
+const withAuth = (opts) => (ComposedComponent) => {
+    const options = {
+        redirectUri: '/',
+        accessRestriction: 'any',
+        ...opts,
+    }
+    class WithAuth extends Component {
+        render() {
+            const props = { ...this.props }
+            return (
+                <AuthContext.Consumer>
+                    {({ isAuthenticated, setAuthenticated }) => {
+                        if ((isAuthenticated && options.accessRestriction === 'notLogged') ||
+                        (!isAuthenticated && options.accessRestriction === 'logged')) {
+                            return <Redirect to={options.redirectUri} />
+                        }
+                        return <ComposedComponent {...props} setAuthenticated={setAuthenticated} />
+                    }}
+                </AuthContext.Consumer>
+            )
+        }
+    }
+
+    return WithAuth
+}
+
+class AuthProviderComponent extends Component {
+    state = {
+        isLoading: false,
+        isAuthenticated: false,
+    }
+    // async componentDidMount() {
+    //     this._isMounted = true
+    //     this.props.call(
+    //         {
+    //             method: 'GET',
+    //             url: 'login',
+    //         },
+    //         () => {
+    //             if (this._isMounted) {
+    //                 this.setState({ isLoading: false, isAuthenticated: true })
+    //             }
+    //         },
+    //         () => {
+    //             if (this._isMounted) {
+    //                 this.setState({ isLoading: false, isAuthenticated: false })
+    //             }
+    //         },
+    //     )
+    // }
+    componentWillUnmount() {
+        this._isMounted = false
+    }
+    setAuthenticated(isAuthenticated) {
+        this.setState({ isAuthenticated })
+    }
+    render() {
+        const { isLoading, isAuthenticated } = this.state
+        if (isLoading) {
+            return null
+        }
+        const props = { ...this.props }
+        delete props.call
+        return (
+            <AuthContext.Provider
+                value={{
+                    isAuthenticated,
+                    setAuthenticated: (isAuth) => this.setAuthenticated(isAuth),
+                }}
+            >
+                {this.props.children}
+            </AuthContext.Provider>
+        )
+    }
+}
+
+AuthProviderComponent.propTypes = {
+    call: PropTypes.func,
+    children: PropTypes.any,
+}
+
+export const AuthProvider = withAPI(AuthProviderComponent)
+
+export default withAuth
---
+++ packages/react-scripts/template/src/routes/Login/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import withAPI from '../../hocs/withAPI'
+import Button from 'components/Button'
+
+class Login extends Component {
+    state = {}
+    handleLogin () {
+        this.props.call(
+            {
+                method: 'post',
+                url: 'login',
+                data: {
+                    username: 'hello',
+                    password: 'ad',
+                }
+            },
+            res => {
+                this.props.setAuthenticated(true)
+            },
+            res => {
+                this.setState({ alert: { type: 'error', message: res.error } })
+            }
+        )
+    }
+    render () {
+        return (
+            <div>
+                <h1>Login</h1>
+                {this.state.alert && <span>{this.state.alert.message}</span>}
+                <Button onClick={() => this.handleLogin()}>Login to this website</Button>
+            </div>
+        )
+    }
+}
+
+Login.propTypes = {
+    call: PropTypes.func,
+    setAuthenticated: PropTypes.func,
+}
+
+export default withAPI(Login)
