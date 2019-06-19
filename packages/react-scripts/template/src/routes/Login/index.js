2118507b76fb22b894ecdffa21ae37411d5747d2
Mehdi GRAR <mehdi.grar@gmail.com>
2019-06-19 17:38:49 +0200

    wip

--- packages/react-scripts/template/src/routes/Login/index.js
+++ packages/react-scripts/template/src/routes/Login/index.js
@@ -4,39 +4,39 @@ import withAPI from 'hocs/withAPI'
 import Button from 'components/Button'
 
 class Login extends Component {
-    state = {}
-    handleLogin () {
-        this.props.call(
-            {
-                method: 'post',
-                url: 'login',
-                data: {
-                    username: 'hello',
-                    password: 'ad',
-                },
-            },
-            res => {
-                this.props.setAuthenticated(true)
-            },
-            res => {
-                this.setState({ alert: { type: 'error', message: res.error } })
-            }
-        )
-    }
-    render () {
-        return (
-            <div>
-                <h1>Login</h1>
-                {this.state.alert && <span>{this.state.alert.message}</span>}
-                <Button onClick={() => this.handleLogin()}>Login to this website</Button>
-            </div>
-        )
-    }
+  state = {}
+  handleLogin () {
+    this.props.call(
+      {
+        method: 'post',
+        url: 'login',
+        data: {
+          username: 'hello',
+          password: 'ad',
+        },
+      },
+      res => {
+        this.props.setAuthenticated(true)
+      },
+      res => {
+        this.setState({ alert: { type: 'error', message: res.error } })
+      }
+    )
+  }
+  render () {
+    return (
+      <div>
+        <h1>Login</h1>
+        {this.state.alert && <span>{this.state.alert.message}</span>}
+        <Button onClick={() => this.handleLogin()}>Login to this website</Button>
+      </div>
+    )
+  }
 }
 
 Login.propTypes = {
-    call: PropTypes.func,
-    setAuthenticated: PropTypes.func,
+  call: PropTypes.func,
+  setAuthenticated: PropTypes.func,
 }
 
 export default withAPI(Login)
