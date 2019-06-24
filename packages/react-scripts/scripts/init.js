fde950de08b249fbfd6638ae48e111f31199e867
Mehdi GRAR <mehdi.grar@gmail.com>
2019-06-24 16:14:18 +0200

    wip

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -234,7 +234,6 @@ module.exports = function(
       return;
     }
   }
-  spawn.sync('yarnpkg', ['add', 'husky@2.4.1', 'lint-staged@8.2.1'])
   if (useTypeScript) {
     verifyTypeScriptSetup();
   }
